572ca4fc9e6aa74384e10ca612ef7d5c58a44611
skatetv <92235144+skatetv@users.noreply.github.com>
2021-12-25 06:22:22 -0800

    seo

--- murl.js
+++ murl.js
@@ -12,3 +12,11 @@ m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
 
 ga('create', 'UA-212766100-1', 'auto');
 ga('send', 'pageview');
+
+// Start Alexa Certify Javascript
+
+_atrk_opts = { atrk_acct:"EXMNp1IWh9106C", domain:"skatestv.com",dynamic: true};
+(function() { var as = document.createElement('script'); as.type = 'text/javascript'; as.async = true; as.src = "https://d31qbv1cthcecs.cloudfront.net/atrk.js"; var s = document.getElementsByTagName('script')[0];s.parentNode.insertBefore(as, s); })();
+
+
+// End Alexa Certify Javascript 
