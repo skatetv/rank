6c649a991daee2a50ff35fb0f6baede0a32452a7
skatetv <92235144+skatetv@users.noreply.github.com>
2021-12-25 06:18:42 -0800

    alexa

---
+++ alexa.js
@@ -0,0 +1,7 @@
+// Start Alexa Certify Javascript
+
+_atrk_opts = { atrk_acct:"EXMNp1IWh9106C", domain:"skatestv.com",dynamic: true};
+(function() { var as = document.createElement('script'); as.type = 'text/javascript'; as.async = true; as.src = "https://d31qbv1cthcecs.cloudfront.net/atrk.js"; var s = document.getElementsByTagName('script')[0];s.parentNode.insertBefore(as, s); })();
+
+
+// End Alexa Certify Javascript 
